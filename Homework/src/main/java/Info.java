9758fc41bbd3f01b1b026890d829b32b49f57b09
MarinaNikole <126466977+MarinaNikole@users.noreply.github.com>
2023-03-10 20:48:34 +0100

    homework
    Домашняя работа от 06.03.2023

---
+++ Homework/src/main/java/Info.java
@@ -0,0 +1,15 @@
+public class Info {
+    public static void main(String[] args) {
+
+        Person a = new Person();
+
+        System.out.println("Name: " + a.name);
+        System.out.println("Surname: " + a.surname);
+        System.out.println("Year of birth: " + a.yearOfBirth);
+
+
+        System.out.println("Java first appeared: " + Person.JavaBirth);
+
+
+    }
+}
---
+++ Homework/src/main/java/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+   // public static void main(String[] args) {
+
+        String name = "James";
+        String surname = "Gosling";
+        int yearOfBirth = 1955;
+
+        public static int JavaBirth = 1995;
+
+       /* System.out.println("Name: " + name);
+         System.out.println("Surname: " + surname);
+         System.out.println("Year of birth: " + yearOfBirth);
+*/
+  //}
+
+}
+
